4290dd2cf7a4537560ed4c66e35c97a07fa6799e
lolzerz <167258079+hyperionlol@users.noreply.github.com>
2024-04-21 01:31:35 +0100

    Create loadstring.lua

---
+++ loadstring.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/hyperionlol/FlameTESTING/main/MainFlame.lua'))()
+wait(120)
+print("FLAME OFFICIALLY RELEASES FOR ANDROID ON THE 16TH OF ???? 2024. hyperionlol.github.io IS THE OFFICIAL SITE. -Hyperion")
+error("Couldn't find FlameUI")
