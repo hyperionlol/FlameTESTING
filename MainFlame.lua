dfd965d28a724c588be11b143f914a7306ae36cf
lolzerz <167258079+hyperionlol@users.noreply.github.com>
2024-04-21 01:27:53 +0100

    Release 0.01
    Doesnt have the actual UI yet as it wont obsfucate for some reason.

---
+++ MainFlame.lua
@@ -0,0 +1,25 @@
+warn("Flame is attempting to fuck the servers..")
+print("FlameUI has been injected successfully by"..identifyexecutor())
+
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+
+Rayfield:Notify({
+    Title = "Smart Hub",
+    Content = "Success! Loading content",
+    Duration = 6.5,
+    Image = 4483362755,
+    Actions = {
+        Ignore = {
+            Name = "Proceed.",
+            Callback = function()
+                print("Flame will now load. This may take a while as it is obsfucated code!")
+            end
+		},
+
+--[[Waiting for the UI to stop obsfucating.. This is taking longer than expected.]]
+--[[Loader by hyperion]]
+--[[
+
+Get Flame @ hyperionlol.github.io
+
+]]
